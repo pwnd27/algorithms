7f067b2d8e142f55f83f5bb422e8060970ef9417
pwnd27 <thebuff27@mail.ru>
2023-03-15 18:01:29 +0700

    add func

---
+++ selection_sort.c
@@ -0,0 +1,37 @@
+#include "stdio.h"
+#include <iso646.h>
+
+#define SIZE 6
+
+void selectionSort(int *arr, int size){
+    int min, min_index, temp, i, j;
+    for (i = 0; i < size; i++){
+        min = arr[i];
+        min_index = i;
+        for (j = i + 1; j < size; j++){
+            if (min > arr[j]){
+                min = arr[j];
+                min_index = j;
+            }
+        }
+        
+        if (min_index != i){
+            temp = arr[i];
+            arr[i] = arr[min_index];
+            arr[min_index] = temp;
+        }
+    }
+}
+
+
+int main(void){
+    int a[SIZE] = {-3, 5, 0, -8, 1, 10};
+    int *p_arr = a;
+    selectionSort(p_arr, SIZE);
+    int i;
+    for (i = 0; i < SIZE; i++){
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    return 0;
+}
